50d471c0c095f494b00b35b0a9c039d5f9871185
Francis Brazeau <hatchet2k4@gmail.com>
2020-04-24 22:20:36 -0400

    Added a bunch of old concept art and zaril's intro song

--- engine/__init__.py
+++ engine/__init__.py
@@ -99,7 +99,67 @@ def Map2Img(mapName, tileset):
                 
     canvas.Save('map2img/' +  mapName + '.png')
 
+def Map2TMX(mapName, tilesetName):
+        
+    ika.Log('Saving map ' + mapName)
+    ika.Map.Switch('maps/' + mapName)
+    
+    twidth = theight = 16
+    
+    mheight = ika.Map.height / theight
+    mwidth = ika.Map.width / twidth
+    
+    #canvas = ika.Canvas(ika.Map.width, ika.Map.height) 
+    s = '''<?xml version="1.0" encoding="UTF-8"?>\n
+<map version="1.2" tiledversion="2020.02.12" orientation="orthogonal" renderorder="right-down" compressionlevel="0" width="''' + str(mwidth) + '''" height="''' + str(mheight) + '''" tilewidth="''' +str(twidth) + '''" tileheight="''' + str(theight) +   '''" infinite="0" nextlayerid="''' + str(ika.Map.layercount + 1) + '''" nextobjectid="1">
+<tileset firstgid="1" source="''' + tilesetName + '''"/> '''
+    
+    for l in range(ika.Map.layercount):                
+        n, w,h, wrapx,wrapy = ika.Map.GetLayerProperties(l)
+        lwidth = w 
+        lheight = h 
+        
+        s += '''\n<layer id="''' +str(l + 1) + '''" name="'''  + n + '''" width="''' +  str(lwidth) + '''" height="''' + str(lheight) +  '''"> 
+        <data encoding="csv">'''          
+        
+        
+        for y in range(int(ika.Map.height/theight)):
+            for x in range(int(ika.Map.width/twidth)):
+            
+                t=ika.Map.GetTile(x,y,l)
+                if t>0: t+=1 #Tile ID's seem to be off by 1 in the TSX tileset, using this to correct. 
+                s+= str(t) + ','
+                
+        s = s[:-1] #hack to remove the last comma from the list
+        s+='\n</data></layer>'
+    s+='\n</map>'
+    
+    file('maps/' + mapName + '.tmx', 'wt').write(s)                    
+    #canvas.Save('map2img/' +  mapName + '.png')
+                    
+        
+def saveallmapsTMX():
+    savemaps=[]
+    #tiles=self.rip_tiles('tiles.png', 16, 16, 6, 503)
+    
+    #hack!
+    try:
+        raw_names = os.listdir('.\\maps')
+        savemaps = []
+        for f in raw_names:
+            if f.endswith('ika-map'):
+                savemaps.append(f)
+        
+        ika.Log( str(savemaps) )
+    except: 
+        #ika.Log(str(e))
+        return None
 
+    ika.Log('maps: ' + str(savemaps))
+    
+    for m in savemaps:
+        #self.Map2Img(m, tiles)    
+        Map2TMX(m, 'Mannux.tsx')
 
 
 class Message(object):
@@ -251,7 +311,7 @@ class Engine(object):
 
     def Run(self):
         self.title.show()
-        #saveallmaps()
+        saveallmapsTMX()
         self.newgame() #only comment out if not showing title
         self.hud.resize()
         self.automap.update_room()
